8c36c10b77067fd60586665f10b6923a3ac26fa8
Joseph Perez <joperez@hotmail.fr>
2026-07-30 13:10:13 +0200

    Remove loom atomics overload
    The SeqCst emulation is abandoned.

--- src/loom.rs
+++ src/loom.rs
@@ -1,87 +1,11 @@
-pub(crate) mod sync {
-    cfg_if::cfg_if! {
-        if #[cfg(loom)] {
-            pub(crate) use loom::sync::*;
-        } else if #[cfg(feature = "std")] {
-            extern crate std;
-            pub(crate) use std::sync::*;
-        }
-    }
-    pub(crate) mod atomic {
-        #[cfg(not(loom))]
-        pub(crate) use core::sync::atomic::*;
-
-        #[cfg(loom)]
-        pub(crate) use loom::sync::atomic::*;
-
-        #[cfg(loom)]
-        fn seqcst_fence(order: Ordering) {
-            if order == loom::sync::atomic::Ordering::SeqCst {
-                loom::sync::atomic::fence(order);
-            }
-        }
-
-        #[cfg(loom)]
-        #[derive(Debug)]
-        pub(crate) struct AtomicPtr<T>(loom::sync::atomic::AtomicPtr<T>);
-
-        #[cfg(loom)]
-        impl<T> AtomicPtr<T> {
-            pub(crate) fn new(ptr: *mut T) -> Self {
-                Self(loom::sync::atomic::AtomicPtr::new(ptr))
-            }
-
-            pub(crate) fn with_mut<R>(&mut self, f: impl FnOnce(&mut *mut T) -> R) -> R {
-                self.0.with_mut(|x| f(x))
-            }
-
-            pub(crate) fn load(&self, order: Ordering) -> *mut T {
-                seqcst_fence(order);
-                self.0.load(order)
-            }
-
-            pub(crate) fn store(&self, x: *mut T, order: Ordering) {
-                self.0.store(x, order);
-                seqcst_fence(order);
-            }
-
-            pub(crate) fn swap(&self, x: *mut T, order: Ordering) -> *mut T {
-                seqcst_fence(order);
-                let res = self.0.swap(x, order);
-                seqcst_fence(order);
-                res
-            }
-
-            pub(crate) fn compare_exchange(
-                &self,
-                current: *mut T,
-                new: *mut T,
-                success: Ordering,
-                failure: Ordering,
-            ) -> Result<*mut T, *mut T> {
-                seqcst_fence(success);
-                let res = self.0.compare_exchange(current, new, success, failure);
-                if res.is_ok() {
-                    seqcst_fence(success);
-                }
-                res
-            }
-
-            pub(crate) fn compare_exchange_weak(
-                &self,
-                current: *mut T,
-                new: *mut T,
-                success: Ordering,
-                failure: Ordering,
-            ) -> Result<*mut T, *mut T> {
-                seqcst_fence(success);
-                let res = self.0.compare_exchange_weak(current, new, success, failure);
-                if res.is_ok() {
-                    seqcst_fence(success);
-                }
-                res
-            }
-        }
+cfg_if::cfg_if! {
+    if #[cfg(loom)] {
+        pub(crate) use loom::sync;
+    } else if #[cfg(feature = "std")] {
+        extern crate std;
+        pub(crate) use std::sync;
+    } else {
+        pub(crate) use core::sync;
     }
 }
 
